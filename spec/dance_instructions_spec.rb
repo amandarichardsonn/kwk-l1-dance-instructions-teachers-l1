62870b93d5aef2ee7ba123eca70f0ebb2984b821
Amanda Richardson <amanda162000@gmail.com>
2018-07-10 15:20:42 +0000

    automatically backed up by learn

--- spec/dance_instructions_spec.rb
+++ spec/dance_instructions_spec.rb
@@ -1,13 +1,13 @@
 # require_relative '../dance_instructions.rb'
-# 
+#
 # RSpec.describe "Gangnam Style" do
-# 
+#
 #   describe "starting_stance" do
 #     it "instructs the user to plant their legs far apart " do
 #       expect { starting_stance }.to output(/Plant legs far apart/).to_stdout
 #     end
 #   end
-# 
+#
 #   describe "skip_step" do
 #     it "instructs the user to skip a step (lower, bounce, briefly) " do
 #       expectation = expect {
@@ -17,14 +17,14 @@
 #       expectation.to output(/Bounce/).to_stdout
 #       expectation.to output(/Briefly/).to_stdout
 #     end
-# 
+#
 #     it "accepts one argument for foot position" do
 #       expect { skip_step }.to raise_error(ArgumentError), 'skip_step should accept 1 argument'
 #     end
-# 
+#
 #   end
-# 
-# 
+#
+#
 #   describe "base_footwork" do
 #     it "instructs the user to lift and return their right foot" do
 #       expectation = expect {
@@ -33,12 +33,12 @@
 #       expectation.to output(/Lift right foot/).to_stdout
 #       expectation.to output(/Return right foot/).to_stdout
 #     end
-# 
+#
 #     it "invokes skip_step method within base_footwork" do
 #       expect { base_footwork }.to output(/Lift right foot/).to_stdout
 #     end
 #   end
-# 
+#
 #   describe "bob_the_reins" do
 #     it "instructs the user to bob it out" do
 #       expectation = expect {
@@ -50,8 +50,8 @@
 #       expectation.to output(/Do/).to_stdout
 #     end
 #   end
-# 
-# 
+#
+#
 #   describe "lasso" do
 #     it "instructs the user to lasso" do
 #       expectation = expect {
@@ -63,15 +63,15 @@
 #       expectation.to output(/Do/).to_stdout
 #     end
 #   end
-# 
-# 
+#
+#
 #   describe "bust_a_move" do
 #     it "instructs the user to bust a move" do
 #       expect { bust_a_move }.to output(/Be cool and be snazzy/).to_stdout
 #     end
 #   end
-# 
-# 
+#
+#
 #   # describe "gangnam_style" do
 #   #   it "instructs the user to bob it out" do
 #   #     expectation = expect {
@@ -83,6 +83,5 @@
 #   #     expectation.to output(//).to_stdout
 #   #   end
 #   # end
-# 
+#
 # end
-
